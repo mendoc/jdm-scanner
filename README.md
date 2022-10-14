d6f8532f21cf87fada9a73f620de74be2de981f1
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2022-10-14 18:11:09 +0000

    Scan un QRCode

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # jdm-scanner
-Une application permettant de scanner les badges des participants aux Journées Digitales de Moanda
+
+Une application permettant de scanner les badges des participants aux Journées Digitales de Moanda. Pour plus d'infrmations visitez [https://jdm2022.events](https://jdm2022.events)
---
+++ funcs/add-name.js
@@ -0,0 +1,60 @@
+require('dotenv').config();
+const axios = require('axios')
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+
+const defaultDocID = "1an_8E_eUJQSM2c10zAASHotbkzm0m9Sjrp-89JwcvK0";
+
+exports.handler = async (event) => {
+
+    console.log(event.queryStringParameters);
+
+    const newName = (event.queryStringParameters.name) || "Aucun nom";
+    const docID = (event.queryStringParameters.doc) || defaultDocID;
+
+    addName(newName, docID);
+
+    return {
+        statusCode: 200,
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        },
+        body: JSON.stringify(event.queryStringParameters)
+    }
+}
+
+async function addName(nameToAdd, cID) {
+    const doc = new GoogleSpreadsheet(cID);
+    await doc.useServiceAccountAuth({
+        client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    });
+    await doc.loadInfo();
+
+    const sheetTitle = (new Date()).toLocaleDateString("fr-FR", { timeZone: "Africa/Libreville" });
+    let sheet = null;
+
+    // Find a sheet which has day as name
+    for (s of doc.sheetsByIndex) {
+        if (s.title == sheetTitle) {
+            sheet = s;
+            break;
+        }
+    }
+
+    // Line to add
+    let ligne = [
+        "=LIGNE()-1",
+        (new Date()).toLocaleString("fr-FR", { timeZone: "Africa/Libreville" }),
+        nameToAdd,
+    ];
+
+    // Create a new sheet if not exists
+    if (!sheet) {
+        sheet = await doc.addSheet({ title: sheetTitle });
+        await sheet.setHeaderRow(["#", "Heure", "Nom"]);
+    }
+
+    // Add a new line with attendee information
+    sheet.addRow(ligne);
+}
